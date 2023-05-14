f5f2fad5c519bde2b236a68290c06a90abfd0001
odikadzegiorgi4@gmail.com <odikadzegiorgi4@gmail.com>
2023-05-14 13:39:19 +0400

    styledHeader

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import { Route } from 'react-router';
 import './App.css';
 import { RoutesComponent } from './Routes';
 import { Header } from './components/header/Header';
+import { Box, styled } from '@mui/material';
+
+const StyledContentContainer = styled(Box)(({theme})=>({
+  [theme.breakpoints.up("sm")]:{
+    marginLeft: "255px",
+  },
+  marginTop: "60px",
+  height: 'calc(100vh - 64px)',
+  padding: 10
+}))
+
 
 function App() {
   return (
-    <>
+    <Box>
       <Header/>
-      <RoutesComponent/>
-
-    </>
+      <StyledContentContainer>
+        <RoutesComponent/>
+      </StyledContentContainer>
+    </Box>
   );
 }
 
